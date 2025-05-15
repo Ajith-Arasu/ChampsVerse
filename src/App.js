9e632f1c452e0d21a3cc77bcd682b4dc10473e74
AmtexRubashree <rubashree.amtexsystems@gmail.com>
2025-05-15 17:00:16 +0530

    global font Family set

--- src/App.js
+++ src/App.js
@@ -19,57 +19,56 @@ import BotWorks from "./Pages/Bot Wroks/work";
 import Quests from "./Pages/Quests/index";
 import Comments from "./Pages/comments/comments";
 import Achievement from "./Pages/Achievement";
-import Events from './Pages/events'
+import Events from './Pages/events';
 import CreateContest from "./Pages/Contests/createContest";
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import '../src/font.css';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Baloo2'", 
+  },
+});
 
 export default function App() {
   const [userDetails, setUserDetails] = useState([]);
   const [profilePic, setProfilePic] = useState("");
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Applies global CSS reset and theme font */}
       <Router>
         <Header userDetails={userDetails} profilePic={profilePic} />
-        <>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/work"
-              element={
-                <Work
-                  setUserDetails={setUserDetails}
-                  setProfilePic={setProfilePic}
-                />
-              }
-            />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/books" element={<Books />} />
-            <Route path="/booksdetail/:userId/:bookId" element={<Detail />} />
-            <Route
-              path="/storage-consumption"
-              element={<StorageConsumption />}
-            />
-            <Route path="/contests" element={<Contests />} />
-            <Route path="/contestdetail" element={<ContestDetail />} />
-            <Route path="/deleted-post" element={<DeletedPost />} />
-            <Route path="/deleted-user" element={<DeletedUser />} />
-            <Route
-              path="/deleted-user/:userId"
-              element={<DeletedUserDetail />}
-            />
-            <Route
-              path="/deleted-user/:userId/works"
-              element={<DeletedUserWorks />}
-            />
-            <Route path="/quests-Works" element={<QuestDetail />} />
-            <Route path="/bot-works" element={<BotWorks />} />
-            <Route path="quests" element={<Quests />} />
-            <Route path="comments" element={<Comments />} />
-              <Route path='/events' element={<Events/>}/>
-            <Route path="/achievements" element={<Achievement />} />
-            <Route path='/createContest/Quest' element={<CreateContest/>}/>
-          </Routes>
-        </>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/work"
+            element={
+              <Work
+                setUserDetails={setUserDetails}
+                setProfilePic={setProfilePic}
+              />
+            }
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/booksdetail/:userId/:bookId" element={<Detail />} />
+          <Route path="/storage-consumption" element={<StorageConsumption />} />
+          <Route path="/contests" element={<Contests />} />
+          <Route path="/contestdetail" element={<ContestDetail />} />
+          <Route path="/deleted-post" element={<DeletedPost />} />
+          <Route path="/deleted-user" element={<DeletedUser />} />
+          <Route path="/deleted-user/:userId" element={<DeletedUserDetail />} />
+          <Route path="/deleted-user/:userId/works" element={<DeletedUserWorks />} />
+          <Route path="/quests-Works" element={<QuestDetail />} />
+          <Route path="/bot-works" element={<BotWorks />} />
+          <Route path="/quests" element={<Quests />} />
+          <Route path="/comments" element={<Comments />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/achievements" element={<Achievement />} />
+          <Route path="/createContest/Quest" element={<CreateContest />} />
+        </Routes>
       </Router>
-    </>
+    </ThemeProvider>
   );
 }
